55cad2ca3d9578abebd902f58a6e99b6e01de6e8
Julio Monteiro <150204882+juliomonteiiro@users.noreply.github.com>
2025-06-07 00:20:40 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,29 @@
 from flask import Flask, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_cors import CORS
+from flask_login import LoginManager
 from dotenv import load_dotenv
 import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 def create_app():
     load_dotenv()
-    app = Flask(__name__, instance_relative_config=True)   
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config.Config')
     app.config.from_pyfile('config.py')
-
+    
     db.init_app(app)
     migrate.init_app(app, db)
-    CORS(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
     @app.after_request
-    def add_no_cache_headers(response: Response):
+    def add_no_cache_headers(response):
+        """Add headers to disable caching for all responses."""
+
         response.cache_control.no_store = True
         response.cache_control.no_cache = True
         response.cache_control.must_revalidate = True
@@ -33,3 +37,8 @@ def create_app():
     app.register_blueprint(home.bp)
 
     return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models import User
+    return User.query.get(int(user_id))
